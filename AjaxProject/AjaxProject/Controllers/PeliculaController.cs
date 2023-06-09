be5b002c60e4769738fda2a4634e0f6c3f71b19d
InformaticNuke <InformaticNuke@gmail.com>
2023-06-08 20:33:42 -0400

    v1

---
+++ AjaxProject/AjaxProject/Controllers/PeliculaController.cs
@@ -0,0 +1,56 @@
+﻿using AjaxProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxProject.Controllers
+{
+    public class PeliculaController : Controller
+    {
+        // GET: Pelicula
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private Pelicula[] peliculas;
+
+        public PeliculaController()
+        {
+
+            peliculas = new Pelicula[]
+            {
+                new Pelicula
+                {
+                    IdPelicula = "100",
+                    NombrePelicula = "John Wick",
+                    CategoriaPelicula = "Accion",
+                    CantidadEntradas = 1,
+                    ValorEntradas = 4500
+                },
+
+                new Pelicula
+                {
+                    IdPelicula = "200",
+                    NombrePelicula = "",
+                    CategoriaPelicula = "Accion",
+                    CantidadEntradas = 1,
+                    ValorEntradas = 4500
+                },
+
+                new Pelicula
+                {
+                    IdPelicula = "300",
+                    NombrePelicula = "John Wick",
+                    CategoriaPelicula = "Accion",
+                    CantidadEntradas = 1,
+                    ValorEntradas = 4500
+                }
+
+            };
+
+        }
+    }
+}
---
+++ AjaxProject/AjaxProject/Models/Pelicula.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxProject.Models
+{
+    public class Pelicula
+    {
+        public string IdPelicula { get; set; }
+        public string NombrePelicula { get; set; }
+        public string CategoriaPelicula { get; set; }
+        public string CantidadEntradas { get; set; }
+        public string ValorEntradas { get; set; }
+    }
+}
